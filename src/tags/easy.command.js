94c5fd7d08067058cf0b4e05e0d2c4eb437ab3d3
Artem Dontsov <extremecodetv@gmail.com>
2018-10-22 23:24:05 +0700

    easy day

---
+++ src/tags/easy.command.js
@@ -0,0 +1,52 @@
+/**
+ * Изи-день работа
+ * Считаем количество дней прошедших без проблем
+ */
+
+const schedule = require('node-schedule');
+
+const { bot, chatId } = require('../config');
+const { send } = require('./send');
+const isTag = require('./../util/isTag');
+
+let counter = 0
+let isEasyDay = false
+let isAlredyEasyDay = false
+
+const getRandomEmoji = (emoji) => {
+    return emoji[Math.floor(Math.random() * emoji.length)];
+};
+
+const getEmoji = (counter) => {
+    const bad = ['🌝','🌩','🔥','💔','🐔','⚓','️☠','️😧','😏','😆','🙀','🐭','🐓']
+    const good = ['🎉']
+
+    if (counter === 0) {
+        return getRandomEmoji(bad)
+    } else {
+        return good[0]
+    }
+}
+
+const shedule = () => {
+    if (isEasyDay) {
+        counter += 1
+    } else {
+        counter = 0
+    }
+
+    const emoji = getEmoji(counter)    
+    bot.sendMessage(chatId, 'Дней без проблем на платформе  — ' + counter + ' ' + emoji)
+}
+
+let job
+if (!job) {
+    job = schedule.scheduleJob({ hour: 6, minute: 00 }, shedule);
+}
+
+const easyHastags = ['изи', 'easy', 'изи_день', 'легчайше'];
+module.exports = async (msg) => {
+    if (isTag(msg.text, easyHastags)) {
+        isEasyDay = isEasyDay || true
+    }
+};
