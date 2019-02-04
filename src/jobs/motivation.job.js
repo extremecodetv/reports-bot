a3ca97192821abf1c9154212135b0059af7e4189
Artem Dontsov <extremecodetv@gmail.com>
2019-02-04 16:41:48 +0700

    add coding motivation

--- src/jobs/motivation.job.js
+++ src/jobs/motivation.job.js
@@ -5,12 +5,12 @@ const schedule = require('node-schedule');
 const { bot, chatId } = require('../config');
 
 const shedule = async () => {
-    const { data } = await axios.get('http://fucking-great-advice.ru/api/random')
-    const text = censore.formattingText(data.text)
+    const { data } = await axios.get('http://fucking-great-advice.ru/api/v2/random-advices-by-tag?tag=coding')
+    const text = censore.formattingText(data.data.shift().text)
     await bot.sendMessage(chatId, text + '\r\n\r\n#совет_дня')
 }
 
 let job
 if (!job) {
-    job = schedule.scheduleJob({ hour: 7, minute: 00 }, () => shedule);
+    job = schedule.scheduleJob({ hour: 7, minute: 00 }, shedule);
 }
